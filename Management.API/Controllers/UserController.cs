9d5e79008b1ae90446a278c1aac05c1201a39efd
Oskar Skak Kristiansen <skakkristiansen@gmail.com>
2018-11-23 10:31:38 +0100

    Update synced with identity user Done

--- Management.API/Controllers/UserController.cs
+++ Management.API/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Management.API.RequestModels;
 using Management.Documents.Documents;
@@ -11,6 +12,7 @@ using Management.Infrastructure.MessagingContracts;
 using Management.Persistence.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 
 namespace Management.API.Controllers
 {
@@ -58,6 +60,30 @@ namespace Management.API.Controllers
         [Route("{id}")]
         public async Task<IActionResult> UpdateUserById(Guid id , [FromBody]  UpdateUserRequestModel userRequestModel)
         {
+            //Calls update controller on Identity API, so the db is synced
+            var identityBaseurl = _identityConfig.Value.IdentityServerUrl + "/identity/users/" + id;
+            var httpClient = new HttpClient();
+            
+            httpClient.DefaultRequestHeaders
+                .Accept
+                .Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            var jsonUpdateStatement = JsonConvert.SerializeObject(new UpdateIdentityUser
+            {
+                Email = userRequestModel.Email,
+                Password = userRequestModel.Password,
+                AuthLevel = userRequestModel.Accesslevel
+            }, Formatting.Indented);
+            
+            var httpContent = new StringContent(jsonUpdateStatement, System.Text.Encoding.UTF8, "application/json");
+            var identityResult = await httpClient.PutAsync(identityBaseurl, httpContent);
+            
+
+            if (!identityResult.IsSuccessStatusCode)
+            {
+                return StatusCode(400, "Identity server could not be reached.");
+            }
+            
+            //Update API db (seperate from identity db)
             var result = await CommandRouter.RouteAsync<UpdateUserCommand, IdResponse>(
                 new UpdateUserCommand(userRequestModel.Name, userRequestModel.Email, userRequestModel.Password,
                     userRequestModel.Accesslevel , id));
---
+++ Management.API/Helpers/UpdateIdentityUser.cs
@@ -0,0 +1,9 @@
+namespace Management.API.Helpers
+{
+    public class UpdateIdentityUser
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int AuthLevel { get; set; }
+    }
+}
