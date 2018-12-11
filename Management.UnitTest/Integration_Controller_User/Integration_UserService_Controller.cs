b4b6c8144cd600a553e7783cbccac35908625a21
Oskar Skak Kristiansen <skakkristiansen@gmail.com>
2018-12-11 11:27:07 +0100

    integration tests changed (not done )

--- Management.UnitTest/Integration_Controller_User/Integration_UserService_Controller.cs
+++ Management.UnitTest/Integration_Controller_User/Integration_UserService_Controller.cs
@@ -20,6 +20,9 @@ namespace Management.UnitTest.User.Controllers
     {
         private IContainer container;
         private UserController userCon;
+        private const string testEmail = "admin@admin.dk";
+
+        //Arrange, Act, Assert
 
         [TestInitialize]
         public void Setup()
@@ -29,83 +32,45 @@ namespace Management.UnitTest.User.Controllers
         }
 
         [TestMethod]
+        public async Task GetAllUsers_NoParam_AssertedTypeOf()
+        {
+            var userList = getUserList().Result;
+            foreach (var user in userList) Assert.IsTrue(user is Persistence.Model.User); 
+        }
+
+        [TestMethod]
         public async Task GetBydId_TestUserAsParam_AssertedEqual()
         {
-            /* var expectedUser = getTestUser();
-             var result = await userCon.GetById(expectedUser.Id) as ObjectResult;
-             var actualUser = result.Value as Management.Persistence.Model.User;
-
-             Assert.AreEqual(expectedUser.Id, actualUser.Id);*/
-            var user1 = await GetTestUser();
-            Assert.IsNotNull(user1);
+            var user = GetTestUser().Result;
+            Assert.IsNotNull(user);
+            Assert.AreEqual(testEmail, user.Email); 
         }
 
+        [TestMethod]
+        public async Task UpdateUser_TestUserAsParam_AssertIsTrue()
+        {
 
-        /// <summary>
-        /// Checks if the user exists in the database, if it does, return the test user
-        /// if not, creates it and then returns it.
-        /// </summary>
-        /// <returns>Test User created in the database</returns>
+        }
+        
         private async Task<Management.Persistence.Model.User> GetTestUser()
         {
-            var testUser = new Management.Persistence.Model.User()
-            {
-                Name = "TESTname",
-                Email = "TEST@gmail.com",
-                AccessLevel = UserRoles.Manager,
-                BaseWage = 100,
-                EmploymentDate = System.DateTime.Parse("2016-11-11T00:00:00")
-            };
-
-            Persistence.Model.User user = CheckIfUserExistsInDB(testUser).Result;
-            if (user != null) return user;
-
-            //Since the user is not in the database, we create one
-            await userCon.CreateUser(new API.RequestModels.CreateUserRequestModel
-            {
-                Name = testUser.Name,
-                Email = testUser.Email,
-                AccessLevel = testUser.AccessLevel,
-                BaseWage = testUser.BaseWage,
-                EmploymentDate = testUser.EmploymentDate,
-                Password = "testpassword"
-            });
-
-            //We confirm it is created, and returns the result, so that the generated id comes with the test user
-            return CheckIfUserExistsInDB(testUser).Result;
-
-           // var user2 = await userCon.GetById(Guid.Parse("dd29557a-7584-48e6-89a0-23dec8025d5d")) as ObjectResult;
-
-
-            /*var allUsers = await userCon.GetALLUsers() as ObjectResult;
-            List<Persistence.Model.User> userList = (List <Persistence.Model.User>)allUsers.Value; 
-
-            foreach (Persistence.Model.User oUser in userList)
-            {
-                if (oUser.Name == testUser.Name && oUser.Email == testUser.Email) return oUser;
-            }
-            
-            var IdObjectResult = await userCon.CreateUser(new API.RequestModels.CreateUserRequestModel
-            {
-                Name = testUser.Name,
-                Email = testUser.Email,
-                AccessLevel = testUser.AccessLevel,
-                Wage = testUser.BaseWage,
-                EmploymentDate = testUser.EmploymentDate,
-                Password = "testpassword"
-            }) as ObjectResult;*/
-            
+            return CheckIfUserExistsInDB(testEmail).Result;
         }
 
-        private async Task<Persistence.Model.User> CheckIfUserExistsInDB(Persistence.Model.User user)
+        private async Task<Persistence.Model.User> CheckIfUserExistsInDB(string userEmail)
         {
-            var allUsers = await userCon.GetALLUsers() as ObjectResult;
-            List<Persistence.Model.User> userList = (List<Persistence.Model.User>)allUsers.Value;
+            var userList = getUserList().Result; 
             foreach(Persistence.Model.User dbUser in userList)
             {
-                if (dbUser.Name == user.Name && dbUser.Email == user.Email) return dbUser;
+                if (dbUser.Email == userEmail) return dbUser;
             }
             return null;
+        }
+
+        private async Task<List<Persistence.Model.User>> getUserList()
+        {
+            var allUsers = await userCon.GetALLUsers() as ObjectResult;
+            return (List<Persistence.Model.User>)allUsers.Value;
         }
 
     }
